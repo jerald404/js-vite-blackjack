9b96890fb823f6c20d5bda532883df9cc24ef0ba
Jerald Vega <xfcgjyfc@gmail.com>
2023-02-20 21:01:46 -0300

    First commit

---
+++ src/blackjack/usecases/crear-deck.js
@@ -0,0 +1,29 @@
+import _ from 'underscore';
+
+/**
+ * 
+ * @param {Array<String>} tiposDeCartas Ejemplo: ['C','D','H','S']
+ * @param {Array<String>} tiposEspeciales Ejemplo: ['A','J','Q','K']
+ * @returns {Array<String>} retorna un nuevo deck de cartas
+ */
+export const crearDeck = (tiposDeCartas, tiposEspeciales) => {
+
+    if (!tiposDeCartas || tiposDeCartas.length === 0) throw new Error('Tipo de carta obligatorio como un arreglo de string');
+
+    let deck = [];
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tiposDeCartas ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tiposDeCartas ) {
+        for( let esp of tiposEspeciales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    return deck;
+}
